3c85a9cba3e787ea2b401b3c37215d599563a7c0
agasthyarkumar <147073909+agasthyarkumar@users.noreply.github.com>
2024-12-25 19:28:16 +0530

    Create leetcodeee.java

---
+++ leetcodeee.java
@@ -0,0 +1,41 @@
+//Find Largest Value in Each Tree Row
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  public List<Integer> largestValues(TreeNode root) {
+    if (root == null)
+      return new ArrayList<>();
+
+    List<Integer> ans = new ArrayList<>();
+    Queue<TreeNode> q = new ArrayDeque<>(List.of(root));
+
+    while (!q.isEmpty()) {
+      int mx = Integer.MIN_VALUE;
+      for (int sz = q.size(); sz > 0; --sz) {
+        TreeNode node = q.poll();
+        mx = Math.max(mx, node.val);
+        if (node.left != null)
+          q.offer(node.left);
+        if (node.right != null)
+          q.offer(node.right);
+      }
+      ans.add(mx);
+    }
+
+    return ans;
+  }
+}
